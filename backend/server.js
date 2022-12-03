2b884c8d19e11720fc8db324723b76a0727c78c1
ruw321 <54829988+ruw321@users.noreply.github.com>
2022-12-03 16:54:30 -0500

    basic set up

--- backend/server.js
+++ backend/server.js
@@ -14,26 +14,26 @@ app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 // Route 1 - register as GET 
 app.get('/hello', routes.hello)
 
-// Route 2 - register as GET 
-app.get('/jersey/:choice', routes.jersey)
+// // Route 2 - register as GET 
+// app.get('/jersey/:choice', routes.jersey)
 
-// Route 3 - register as GET 
-app.get('/matches/:league', routes.all_matches)
+// // Route 3 - register as GET 
+// app.get('/matches/:league', routes.all_matches)
 
-// Route 4 - register as GET 
-app.get('/players', routes.all_players)
+// // Route 4 - register as GET 
+// app.get('/players', routes.all_players)
 
-// Route 5 - register as GET 
-app.get('/match', routes.match)
+// // Route 5 - register as GET 
+// app.get('/match', routes.match)
 
-// Route 6 - register as GET 
-app.get('/player', routes.player)
+// // Route 6 - register as GET 
+// app.get('/player', routes.player)
 
-// Route 7 - register as GET 
-app.get('/search/matches', routes.search_matches)
+// // Route 7 - register as GET 
+// app.get('/search/matches', routes.search_matches)
 
-// Route 8 - register as GET 
-app.get('/search/players', routes.search_players)
+// // Route 8 - register as GET 
+// app.get('/search/players', routes.search_players)
 
 
 
