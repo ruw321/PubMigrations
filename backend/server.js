4d9056844c10472ace3a6059888320740b0c7ba9
Radin Nojoomi <ranojoomi@gmail.com>
2022-12-03 22:53:07 -0500

    updated routes with few endpoint implementations

--- backend/server.js
+++ backend/server.js
@@ -8,36 +8,23 @@ const config = require('./config.json')
 
 const app = express();
 
-// whitelist localhost 3000
-app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
+// whitelist localhost 8000
+app.use(cors({ credentials: true, origin: ['http://localhost:8000'] }));
 
 // Route 1 - register as GET 
 app.get('/hello', routes.hello)
 
-// // Route 2 - register as GET 
-// app.get('/jersey/:choice', routes.jersey)
+// Query 13 - register as GET
+app.get('/getBestAuthors', routes.getBestAuthors)
 
-// // Route 3 - register as GET 
-// app.get('/matches/:league', routes.all_matches)
+// Query 12 - register as GET
+app.get('/mostEmployedCities', routes.mostEmployedCities)
 
-// // Route 4 - register as GET 
-// app.get('/players', routes.all_players)
+// Query 11 - register as GET
+app.get('/mostBenefitedOrg', routes.mostBenefitedOrg)
 
-// // Route 5 - register as GET 
-// app.get('/match', routes.match)
-
-// // Route 6 - register as GET 
-// app.get('/player', routes.player)
-
-// // Route 7 - register as GET 
-// app.get('/search/matches', routes.search_matches)
-
-// // Route 8 - register as GET 
-// app.get('/search/players', routes.search_players)
-
-
-
-
+// Query 10 - register as GET
+app.get('/mostBenefitedOrg', routes.mostBenefitedOrg)
 
 app.listen(config.server_port, () => {
     console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
